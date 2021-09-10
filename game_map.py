448d3a3128979413381db916cb4236b7e753641c
FilledWith <alex.v.morkovin@gmail.com>
2021-09-10 23:22:22 +0300

    refactoring

--- game_map.py
+++ game_map.py
@@ -26,6 +26,10 @@ class GameMap:
         self.explored = np.full(
             (width, height), fill_value=False, order="F"
         )  # Tiles the player has seen before
+ 
+    @property
+    def gamemap(self) -> GameMap:
+        return self
     
     @property
     def actors(self) -> Iterator[Actor]:
